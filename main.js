48d2179cbd69d01f39f8a90b55526c047f238980
LiaAnika <anika.chavda@yahoo.com>
2017-04-27 11:18:40 +0100

    main update

--- main.js
+++ main.js
@@ -3,18 +3,56 @@ var mainStage = {
  preload: function() {
  //this function will be executed at the beginning 
      //that's where we load the images and sound
+     
+     //load the bird
+     game.load.image('bird', 'assets/bird.png');
  },
     create: function() {
     // this function is called after the perload 
-        // here we setup the game
+    // here we setup the game
     
+        // change the background colour to blue 
+        game.state.backgroundColor = '#71c5cf';
+        
+        //set the physics 
+        game.physics.startsystem(phaser.physics.arcade);
+        
+        //bird
+        this.bird = game.add.sprite(100,245, 'bird');
+        
+        
+        game.physics.arcade.enable(this.bird);
+        
+        //add gravity 
+        this.bird.body.gravity.y =1000;
+        
+        //control 
+        
+        var spaceBar = game.input.keyboard.addkey(             Phaser.keyboard.SPACEBAR);
+        spaceBar.ondown.add(this.jump, this)
     }, 
     
     update:function() { 
     //this function is called 60 times per second 
     //it contains the game logic    
     
+        
+        //call the restaertgame 
+        
+        if (this.bird.y <0 || this.bird.y >490)
+            this.restartGame(); 
     },
+    
+    jump:function() {
+        //add a vertical velocity 
+        this.bird.body.velocity.y =-350;
+    },
+    
+    
+    //restart game 
+    restartGame: function() {
+    game.state.start('main');
+    ], 
 };
 
 //phaser
